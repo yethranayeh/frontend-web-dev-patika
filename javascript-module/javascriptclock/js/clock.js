935ab6719b997c546e66af89a932bb1fc71a5a6a
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2021-10-11 09:54:09 +0300

    current time and day added, bound to an interval

--- javascript-module/javascriptclock/js/clock.js
+++ javascript-module/javascriptclock/js/clock.js
@@ -1,8 +1,12 @@
 /** @format */
 
 const username = document.getElementById("myName");
-username.innerHTML = `test`;
+username.innerHTML = prompt("Your name?", "Name");
 
+let timer = setInterval(showTime, 1000);
 function showTime() {
-	console.log("func called");
+	let clock_time = new Date().toLocaleTimeString().substr(0, 7);
+	let day = new Date().toLocaleString(window.navigator.language, { weekday: "long" });
+	const clock = document.getElementById("myClock");
+	clock.innerHTML = `${clock_time} ${day}`;
 }
